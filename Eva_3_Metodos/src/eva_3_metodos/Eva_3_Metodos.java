726f447f5598ccb59afeae2599ce33b11fac9b17
LiraStrongs <sebastianlira06@gmail.com>
2024-11-04 17:41:20 -0600

    Add files via upload

---
+++ Eva_3_Metodos/src/eva_3_metodos/Eva_3_Metodos.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_metodos;
+
+/**
+ *
+ * @author sebastianlira
+ */
+public class Eva_3_Metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //el public y el static son modificadores, los metodos pertenecen a una clase
+        
+             imprimirMensaje("Hola mundo!!!");
+        
+    }
+   //header (encabezado)
+    //Imprimir un mensaje
+    // JAVA:iniciar en minusculas, siguientes palabras inician en mayusculas.
+    //declaracion
+    // el suo de los metodos se dice que lo invocas o llamas
+    
+    public static void imprimirMensaje(String mensaje){
+    
+    System.out.println("tu mensaje"+ mensaje);
+    
+    
+    }
+    
+    
+    
+    
+    
+}
