05e1564e9c982fe73a4529f3450f008991d6c2ce
LiraStrongs <sebastianlira06@gmail.com>
2024-11-05 17:07:18 -0600

    Add files via upload

---
+++ EVA_3_Metodoss/src/eva_3_metodoss/EVA_3_Metodoss.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_metodoss;
+
+/**
+ *
+ * @author sebastianlira
+ */
+public class EVA_3_Metodoss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int resu;
+        resu = sumarDosNumeros(5,10);
+        System.out.println("resultado ="+ resu);
+        
+        
+        
+    }
+    
+    
+    public static int sumarDosNumeros(int num1, int num2){
+    
+   
+    int suma;
+    suma = num1 + num2;
+    return suma;
+    
+    
+    }
+}
